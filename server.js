356d55c462da103ac4f5282f3ed1638ca9df4723
Napoleon Oikonomou <napoleonoikon@gmail.com>
2021-03-30 02:54:03 +0300

    add favicon, add some tests

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import "dotenv/config.js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 import express from "express";
 import morgan from "morgan";
 import compression from "compression";
+import favicon from "serve-favicon";
 import cors from "cors";
 import chalk from "chalk";
 import Sentry from "@sentry/node";
@@ -18,12 +21,15 @@ const app = express();
 app.use(helmet());
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.errorHandler());
-if (process.env.NODE_ENV === "development") app.use(morgan("dev", { skip: (req) => req.method === "OPTIONS" }));
+app.use(morgan(process.env.NODE_ENV === "development" ? "dev" : "short", {
+	skip: (req) => process.env.NODE_ENV === "test" || req.originalUrl === "/favicon.ico" || req.method === "OPTIONS",
+}));
 app.use(cookieParser());
 app.use(cors({ credentials: true, origin: true }));
 app.use(compression());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+app.use(favicon(path.join(path.dirname(fileURLToPath(import.meta.url)), "src", "assets", "images", "favicon.ico")));
 
 app.use(routes);
 
--- tests/init.test.js
+++ tests/init.test.js
@@ -1,3 +1,28 @@
-import test from "ava";
+import "dotenv/config.js";
+import http from "http";
 
-test.todo("main");
+import test from "ava";
+import got from "got";
+import listen from "test-listen";
+
+import app from "../server.js";
+
+test.before(async (t) => {
+	t.context.server = http.createServer(app);
+	const prefixUrl = await listen(t.context.server);
+	t.context.got = got.extend({ throwHttpErrors: false, responseType: "json", prefixUrl });
+});
+
+test.after.always((t) => t.context.server.close());
+
+test("GET /ping/ returns correct response and status code", async (t) => {
+	const { body, statusCode } = await t.context.got("ping");
+	t.is(body.message, "pong");
+	t.is(statusCode, 200);
+});
+
+test("GET /whatever/ returns 404", async (t) => {
+	const { body, statusCode } = await t.context.got("whatever");
+	t.is(body.message, "Page not found 😞");
+	t.is(statusCode, 404);
+});
