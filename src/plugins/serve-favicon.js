2b91e9e3ac67b7b6be7b63158ebe7fa816390f1e
Napoleon-Christos Oikonomou <napoleonoikon@gmail.com>
2021-05-12 11:44:36 +0300

    update deps

--- src/plugins/serve-favicon.js
+++ src/plugins/serve-favicon.js
@@ -1,5 +1,5 @@
-import { readFile } from "fs/promises";
-import { createHash } from "crypto";
+import { readFile } from "node:fs/promises";
+import { createHash } from "node:crypto";
 
 import plugin from "fastify-plugin";
 
--- tests/init.test.js
+++ tests/init.test.js
@@ -1,4 +1,4 @@
-import "dotenv/config.js";
+import "dotenv/config";
 
 import test from "ava";
 import got from "got";
